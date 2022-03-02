d434396bdbf04544ea60eac5665f3c4a8a85cadb
Neil Angajala <neilangajala24@gmail.com>
2022-03-01 21:52:46 -0600

    login button routes to new page successfully

--- home_train/lib/main.dart
+++ home_train/lib/main.dart
@@ -85,7 +85,11 @@ class _MyHomePageState extends State<MyHomePage> {
               Positioned(
               bottom: 150, 
               right: 25,
-              child: TextButton(onPressed:(){},child:Container(
+              child: TextButton(onPressed:(){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Signin_Builder()),
+                );
+              },child:Container(
                 height: 50, 
                 width: 250,
                 color: Colors.transparent,
@@ -129,53 +133,36 @@ class _MyHomePageState extends State<MyHomePage> {
           ]
         )
       );
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     // Here we take the value from the MyHomePage object that was created by
-    //     // the App.build method, and use it to set our appbar title.
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     // Center is a layout widget. It takes a single child and positions it
-    //     // in the middle of the parent.
-    //     child: Column(
-    //       // Column is also a layout widget. It takes a list of children and
-    //       // arranges them vertically. By default, it sizes itself to fit its
-    //       // children horizontally, and tries to be as tall as its parent.
-    //       //
-    //       // Invoke "debug painting" (press "p" in the console, choose the
-    //       // "Toggle Debug Paint" action from the Flutter Inspector in Android
-    //       // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-    //       // to see the wireframe for each widget.
-    //       //
-    //       // Column has various properties to control how it sizes itself and
-    //       // how it positions its children. Here we use mainAxisAlignment to
-    //       // center the children vertically; the main axis here is the vertical
-    //       // axis because Columns are vertical (the cross axis would be
-    //       // horizontal).
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         const Text(
-    //           'You have pushed the button this many times:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headline4,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
+  }
+}
+
+class Signin_Builder extends StatefulWidget{
+  const Signin_Builder({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _signin();
+  
+}
+class _signin extends State<Signin_Builder>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          Align(
+            alignment: FractionalOffset.center,
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                border: Border.all(
+                  color: const Color.fromRGBO(0, 93, 92, 1.0),
+                  width: 3
+                )
+              ),
+            )
+            )
+        ]
+      )
+    );
   }
 }
