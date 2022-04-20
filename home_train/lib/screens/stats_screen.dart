fad70113f3b602567048333dabce5dd859c9d7a0
Jonathan Sneh <jonathaninterweb@gmail.com>
2022-04-20 16:21:01 -0500

    added const when necessary and changed stats page

--- home_train/lib/screens/stats_screen.dart
+++ home_train/lib/screens/stats_screen.dart
@@ -27,38 +27,39 @@ class _StatsScreen extends State<StatsScreen> {
     var size = MediaQuery.of(context).size;
     String topText = widget.workout + " Analytics";
     List<Color> gradientColors = [
-    const Color(0xff23b6e6),
-    const Color(0xff02d39a),
-  ];
+      homeTrainBlue,
+      homeTrainGreen,
+    ];
     return Scaffold(
         body: Column(children: [
       GenericBanner(
         homeTrainBlue,
         text: topText,
       ),
-      SizedBox(height : 20,),
+      const SizedBox(
+        height: 20,
+      ),
       Padding(
         padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Container (
+        child: Container(
           width: double.infinity,
           height: 250,
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.01),
-                spreadRadius: 10,
-                blurRadius: 3
-              )
-            ]),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children:[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: Column(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.01),
+                    spreadRadius: 10,
+                    blurRadius: 3)
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Column(
                       //crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         // Text("Activity", style: TextStyle(
@@ -68,103 +69,100 @@ class _StatsScreen extends State<StatsScreen> {
                         // )),
                         // SizedBox(height: 10,
                         // ),
-                        Text(
-                          "Average " + widget.workout + "/day: 13",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          )
+                        Text("Average " + widget.workout + "/day: 13",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            )),
+                        const SizedBox(height: 10)
+                      ]),
+                ),
+                Container(
+                  width: size.width - 20,
+                  height: 150,
+                  child: LineChart(
+                      /////////////////***********CITATION: https://www.codeplayon.com/2021/10/how-to-make-line-charts-in-flutter/*****************///////////////// */
+                      LineChartData(
+                    gridData: FlGridData(
+                      show: true,
+                      drawVerticalLine: true,
+                      horizontalInterval: 1,
+                      verticalInterval: 1,
+                      getDrawingHorizontalLine: (value) {
+                        return FlLine(
+                          color: const Color(0xff37434d),
+                          strokeWidth: 1,
+                        );
+                      },
+                      getDrawingVerticalLine: (value) {
+                        return FlLine(
+                          color: const Color(0xff37434d),
+                          strokeWidth: 1,
+                        );
+                      },
+                    ),
+                    // titlesData: FlTitlesData(
+                    //   show: true,
+                    //   rightTitles: AxisTitles(
+                    //     sideTitles: SideTitles(showTitles: false),
+                    //   ),
+                    //   topTitles: AxisTitles(
+                    //     sideTitles: SideTitles(showTitles: false),
+                    //   ),
+                    //   bottomTitles: AxisTitles(
+                    //     sideTitles: SideTitles(
+                    //       showTitles: true,
+                    //       reservedSize: 30,
+                    //       interval: 1,
+                    //       getTitlesWidget: bottomTitleWidgets,
+                    //     ),
+                    //   ),
+                    //   leftTitles: AxisTitles(
+                    //     sideTitles: SideTitles(
+                    //       showTitles: true,
+                    //       interval: 1,
+                    //       getTitlesWidget: leftTitleWidgets,
+                    //       reservedSize: 42,
+                    //     ),
+                    //   ),
+                    // ),
+                    borderData: FlBorderData(
+                        show: true,
+                        border: Border.all(
+                            color: const Color(0xff37434d), width: 1)),
+                    minX: 0,
+                    maxX: 11,
+                    minY: 0,
+                    maxY: 6,
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: [
+                          FlSpot(0, 3),
+                          FlSpot(2.6, 2),
+                          FlSpot(4.9, 5),
+                          FlSpot(6.8, 3.1),
+                          FlSpot(8, 4),
+                          FlSpot(9.5, 3),
+                          FlSpot(11, 4),
+                        ],
+                        isCurved: true,
+                        colors: gradientColors,
+                        barWidth: 5,
+                        isStrokeCapRound: true,
+                        dotData: FlDotData(
+                          show: false,
                         ),
-                        SizedBox(height: 10)
-                      ]
-                    ),
-                  ),
-                  Container(
-                    width: size.width - 20,
-                    height: 150,
-                    child: LineChart(
-                        /////////////////***********CITATION: https://www.codeplayon.com/2021/10/how-to-make-line-charts-in-flutter/*****************///////////////// */
-                        LineChartData(
-                          gridData: FlGridData(
-                            show: true,
-                            drawVerticalLine: true,
-                            horizontalInterval: 1,
-                            verticalInterval: 1,
-                            getDrawingHorizontalLine: (value) {
-                              return FlLine(
-                                color: const Color(0xff37434d),
-                                strokeWidth: 1,
-                              );
-                            },
-                            getDrawingVerticalLine: (value) {
-                              return FlLine(
-                                color: const Color(0xff37434d),
-                                strokeWidth: 1,
-                              );
-                            },
-                          ),
-                          // titlesData: FlTitlesData(
-                          //   show: true,
-                          //   rightTitles: AxisTitles(
-                          //     sideTitles: SideTitles(showTitles: false),
-                          //   ),
-                          //   topTitles: AxisTitles(
-                          //     sideTitles: SideTitles(showTitles: false),
-                          //   ),
-                          //   bottomTitles: AxisTitles(
-                          //     sideTitles: SideTitles(
-                          //       showTitles: true,
-                          //       reservedSize: 30,
-                          //       interval: 1,
-                          //       getTitlesWidget: bottomTitleWidgets,
-                          //     ),
-                          //   ),
-                          //   leftTitles: AxisTitles(
-                          //     sideTitles: SideTitles(
-                          //       showTitles: true,
-                          //       interval: 1,
-                          //       getTitlesWidget: leftTitleWidgets,
-                          //       reservedSize: 42,
-                          //     ),
-                          //   ),
-                          // ),
-                          borderData: FlBorderData(
-                              show: true,
-                              border: Border.all(color: const Color(0xff37434d), width: 1)),
-                          minX: 0,
-                          maxX: 11,
-                          minY: 0,
-                          maxY: 6,
-                          lineBarsData: [
-                            LineChartBarData(
-                              spots: [
-                                FlSpot(0, 3),
-                                FlSpot(2.6, 2),
-                                FlSpot(4.9, 5),
-                                FlSpot(6.8, 3.1),
-                                FlSpot(8, 4),
-                                FlSpot(9.5, 3),
-                                FlSpot(11, 4),
-                              ],
-                              isCurved: true,
-                              colors: gradientColors,
-                              barWidth: 5,
-                              isStrokeCapRound: true,
-                              dotData: FlDotData(
-                                show: false,
-                              ),
-                              belowBarData: BarAreaData(
-                                show: true,
-                                colors: gradientColors,
-                              ),
-                            ),
-                          ],
-                        )
-                    ),
-                  )
-                ],
-              ),
+                        belowBarData: BarAreaData(
+                          show: false,
+                          colors: gradientColors,
+                        ),
+                      ),
+                    ],
+                  )),
+                )
+              ],
             ),
+          ),
         ),
       ),
       SizedBox(height: 20),
@@ -172,77 +170,62 @@ class _StatsScreen extends State<StatsScreen> {
         spacing: 20,
         children: [
           Container(
-            width: (size.width-60)/2,
-            height: 170,
-            decoration: BoxDecoration(
-              color: homeTrainBlue,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [BoxShadow(
-                color: Colors.grey.withOpacity(0.01),
-                spreadRadius: 10,
-                blurRadius: 3
-              )]
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 25, right: 25, top: 20, bottom: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: homeTrainGreen,
-                      shape: BoxShape.circle
-                    ),
-                    child: Icon(
-                      Icons.arrow_back,
-                      color: Colors.white
-                    )
-                  )
-                ],
-              )
-            )
-          ),
+              width: (size.width - 60) / 2,
+              height: 170,
+              decoration: BoxDecoration(
+                  color: homeTrainBlue,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.01),
+                        spreadRadius: 10,
+                        blurRadius: 3)
+                  ]),
+              child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 25, top: 20, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                              color: homeTrainGreen, shape: BoxShape.circle),
+                          child: Icon(Icons.arrow_back, color: Colors.white))
+                    ],
+                  ))),
           Container(
-            width: (size.width-60)/2,
-            height: 170,
-            decoration: BoxDecoration(
-              color: homeTrainGreen,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [BoxShadow(
-                color: Colors.grey.withOpacity(0.01),
-                spreadRadius: 10,
-                blurRadius: 3
-              )]
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 25, right: 25, top: 20, bottom: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: homeTrainBlue,
-                      shape: BoxShape.circle
-                    ),
-                    child: Icon(
-                      Icons.arrow_forward,
-                      color: Colors.white
-                    )
-                  )
-                ],
-              )
-            )
-          ),
+              width: (size.width - 60) / 2,
+              height: 170,
+              decoration: BoxDecoration(
+                  color: homeTrainGreen,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.01),
+                        spreadRadius: 10,
+                        blurRadius: 3)
+                  ]),
+              child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 25, top: 20, bottom: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          width: 40,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                              color: homeTrainBlue, shape: BoxShape.circle),
+                          child: const Icon(Icons.arrow_forward,
+                              color: Colors.white))
+                    ],
+                  ))),
         ],
-      ) 
-
-      //TODO: Add Stats Display/Info
-
+      )
     ]));
   }
 }
 
+//TODO: Generalize to show all users' stats
